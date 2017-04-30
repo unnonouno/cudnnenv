dd1938f45c78a1f60874d82795772186c73f4b3e
Yuya Unno <unnonouno@gmail.com>
2017-04-30 19:18:30 +0900

    Update version and change log

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='cudnnenv',
-    version='0.4.0',
+    version='0.5.0',
     description='cudnn environment manager',
     long_description=open('README.rst').read(),
     author='Yuya Unno',
